e2e73f7d9f0a999ff09ccb7706cbc7a1bf0ed03d
Sohail Saha <captainwoof.official@gmail.com>
2021-11-01 20:51:40 +0530

    Imported navbar in all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
+import Navbar from "../components/molecules/navbar"
 import StyledThemeProvider from "../components/providers/theme"
 
 function MyApp({ Component, pageProps }) {
   return (
     <StyledThemeProvider>
+      <Navbar />
       <Component {...pageProps} />
     </StyledThemeProvider>
   )
