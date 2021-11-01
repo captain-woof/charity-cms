4f287a0418535a048728acfebca5082154556678
Sohail Saha <captainwoof.official@gmail.com>
2021-11-01 20:50:42 +0530

    Edited link style

--- components/atoms/link.tsx
+++ components/atoms/link.tsx
@@ -3,9 +3,9 @@ import LinkNext from 'next/link'
 import { ChildrenProp, InlineStyled } from '../../types/comps'
 
 const Anchor = styled.a`
-    ${({ theme }) => (css`
+    ${({ theme, color }) => (css`
         text-decoration: none;
-        color: ${theme.colors.secondary.dark};
+        color: ${color || theme.colors.secondary.main};
         width: fit-content;
         transition: ${theme.transition('color').normal};
         position: relative;
@@ -13,9 +13,11 @@ const Anchor = styled.a`
         width: fit-content;
         cursor: pointer;
         display: inline-flex;
+        font-weight: 500;
+        font-family: ${theme.font.family.secondary};
 
         &:hover {
-            color: ${theme.colors.primary.main};
+            color: ${color || theme.colors.secondary.dark};
         }
 
         &::after {
@@ -24,7 +26,7 @@ const Anchor = styled.a`
             position: absolute;
             width: 100%;
             height: 0.125rem;
-            background-color: ${theme.colors.primary.main};
+            background-color: ${color || theme.colors.primary.main};
             transition: ${theme.transition('left').normal};
             z-index: -1;
             left: -100%;
@@ -41,26 +43,19 @@ const Anchor = styled.a`
 interface ILink extends ChildrenProp, InlineStyled {
     id?: string
     href: string
+    color?: string
 }
 
-export const Link = ({ id, href, children, style }: ILink) => {
+export const Link = ({ id, href, children, style, color }: ILink) => {
     return (
         <LinkNext href={href}>
-            <Anchor id={id} style={style}>{children}</Anchor>
+            <Anchor id={id} style={style} color={color}>{children}</Anchor>
         </LinkNext>
     )
 }
 
-export const LinkBlock = ({ id, href, children, style }: ILink) => {
+export const LinkExternal = ({ id, href, children, style, color }: ILink) => {
     return (
-        <LinkNext href={href}>
-            <a id={id} style={style}>{children}</a>
-        </LinkNext>
-    )
-}
-
-export const LinkExternal = ({ id, href, children, style }: ILink) => {
-    return (
-        <Anchor id={id} href={href} target='_blank' style={style}>{children}</Anchor>
+        <Anchor id={id} href={href} target='_blank' style={style} color={color}>{children}</Anchor>
     )
 }
