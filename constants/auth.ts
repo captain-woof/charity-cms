982380a2d0631a3fac686cd01d144886ce5cdd02
Sohail Saha <captainwoof.official@gmail.com>
2021-11-02 11:48:20 +0530

    One of the auth error messages changed

--- constants/auth.ts
+++ constants/auth.ts
@@ -3,7 +3,7 @@ const AuthErrorMessages = {
     "auth/expired-action-code": "The password reset code you provided has expired.",
     "auth/invalid-action-code": "The password reset code you provided is either invalid or has already been used.",
     "auth/user-disabled": "The user account has been disabled by an administrator.",
-    "auth/user-not-found": "There is no user record corresponding to this identifier. The user may have been deleted.",
+    "auth/user-not-found": "No user found with these credentials.",
     "auth/weak-password": "The password you provided is too weak!",
     "auth/email-already-in-use": "The email address is already in use by another account.",
     "auth/invalid-email": "The email address is incorrectly formatted.",
