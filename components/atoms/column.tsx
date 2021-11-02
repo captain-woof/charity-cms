5079c3cb6a1260016d353fcff974c8d71c0f6a1c
Sohail Saha <captainwoof.official@gmail.com>
2021-11-02 13:10:33 +0530

    Added column atomic comp

---
+++ components/atoms/column.tsx
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components'
+import { ChildrenProp, InlineStyled } from '../../types/comps'
+
+interface IColumn extends ChildrenProp, InlineStyled{
+    wrap?: boolean
+    hCenter?: boolean
+    vCenter?: boolean
+}
+
+const StyledColumn = styled.div<IColumn>`
+    ${({ wrap, hCenter, vCenter }) => css`
+        display: flex;
+        flex-direction: column;
+        flex-wrap: ${wrap ? 'wrap' : 'nowrap'};
+        width: max-content;
+        justify-content: ${hCenter ? 'center' : 'auto'};
+        align-items: ${vCenter ? 'center' : 'auto'};
+    `}
+`
+
+export default function Column({ children, wrap = true, style, hCenter = false, vCenter = false }: IColumn) {
+    return (
+        <StyledColumn wrap={wrap} style={style} vCenter={vCenter} hCenter={hCenter}>
+            {children}
+        </StyledColumn>
+    )
+}
