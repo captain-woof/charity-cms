714a7a7fe044361ec4900bed4f7a4bb19478e1c2
Sohail Saha <captainwoof.official@gmail.com>
2021-11-03 10:38:20 +0530

    Integrated formik in login form

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,16 +1,19 @@
-import Textfield from "../../components/atoms/textfield"
+import TextfieldFormik from "../../components/atoms/textfield-formik"
 import { GetServerSideProps, GetServerSidePropsContext } from 'next'
 import { parseCookies } from 'nookies'
 import { verifyIdToken } from "../../utils/auth-server"
 import { Container } from "../../components/atoms/container"
 import styled, { css, useTheme } from "styled-components"
 import { Heading4 } from "../../components/atoms/headings"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import Button from "../../components/atoms/button"
 import { BsGoogle } from 'react-icons/bs'
 import { Link } from "../../components/atoms/link"
 import { useOAuth, useSignInWithEmailAndPassword, useUser } from "../../hooks/auth"
 import { useRouter } from "next/router"
+import * as yup from 'yup'
+import { Form, Formik } from 'formik'
+import ErrorMessage from "../../components/atoms/error-message"
 
 // Check if user is already logged in (Server side)
 export const getServerSideProps: GetServerSideProps = async (ctx: GetServerSidePropsContext) => {
@@ -51,7 +54,7 @@ const FormBox = styled.main`
     `}
 `
 
-const LoginForm = styled.form`
+const LoginForm = styled(Form)`
     width: 100%;
     padding: var(--sp-400);
     padding-bottom: var(--sp-200);
@@ -66,12 +69,27 @@ const OAuthContainer = styled(LoginForm)`
     align-items: center;
 `
 
+/* Form validation */
+const validationSchema = yup.object({
+    email: yup
+        .string()
+        .email("Invalid email address")
+        .required("Required"),
+    password: yup
+        .string()
+        .required("Required"),
+})
+
+/* Form initial values */
+const initialValues = {
+    email: '',
+    password: ''
+}
+
 export default function Login() {
     const router = useRouter()
     const theme = useTheme()
-    const [email, setEmail] = useState<string>('')
-    const [password, setPassword] = useState<string>('')
-    const { signIn: signInWithEmailAndPassword, pending } = useSignInWithEmailAndPassword()
+    const { signIn: signInWithEmailAndPassword, pending, error: loginError } = useSignInWithEmailAndPassword()
     const { signIn: signInWithOAuth } = useOAuth()
     const user = useUser()
 
@@ -90,58 +108,50 @@ export default function Login() {
             flexDirection: 'column',
             gap: 'var(--sp-400) 0'
         }}>
-            <FormBox>
-                <Heading4 style={{
-                    fontWeight: 600
-                }}>
-                    Login
-                </Heading4>
-                <LoginForm onSubmit={(e) => {
-                    e.preventDefault();
+            <Formik validationSchema={validationSchema} initialValues={initialValues}
+                onSubmit={({ email, password }, { setSubmitting }) => {
                     signInWithEmailAndPassword(email, password)
+                    setSubmitting(false)
                 }}>
-                    <Textfield label='Email' name='email' inputProps={{
-                        onChange: (e) => { setEmail(e.target.value) },
-                        type: 'email',
-                        required: 'true'
-                    }} />
-                    <Textfield label='Password' name='password' inputProps={{
-                        onChange: (e) => { setPassword(e.target.value) },
-                        type: 'password',
-                        required: 'true'
-                    }} />
-                    <Button disabled={pending || !(email && password)} buttonProps={{
-                        type: 'submit'
-                    }}>
-                        Login
-                    </Button>
-
-                </LoginForm>
-                <Link href="/auth/forgot-password" color={theme.colors.primary.main} style={{
-                    fontSize: 'var(--fs-300)',
-                    alignSelf: 'flex-end',
-                    marginRight: 'var(--sp-300)'
-                }}>
-                    Forgot password ?
-                </Link>
-                <OAuthContainer>
-                    <p style={{
-                        color: theme.colors.black.light,
-                        fontWeight: 600,
-                        fontSize: 'var(--fs-400)',
-                        fontFamily: theme.font.family.secondary
-                    }}>
-                        Or
-                    </p>
-                    <Button iconStart={<BsGoogle />} style={{
-                        backgroundColor: '#DB4437'
-                    }} buttonProps={{
-                        onClick: (e) => { e.preventDefault(); signInWithOAuth('google') }
-                    }}>
-                        Login with Google
-                    </Button>
-                </OAuthContainer>
-            </FormBox>
+                {({ handleSubmit, isValid, isSubmitting }) => (
+                    <FormBox>
+                        <Heading4 style={{ fontWeight: 600 }}>Login</Heading4>
+                        <LoginForm onSubmit={(e) => {
+                            e.preventDefault();
+                            handleSubmit()
+                        }}>
+                            <TextfieldFormik label='Email' name='email' />
+                            <TextfieldFormik label='Password' name='password' />
+                            <Button disabled={pending || (!isValid) || (isSubmitting)} buttonProps={{
+                                type: 'submit'
+                            }}>Login</Button>
+                        </LoginForm>
+                        <Link href="/auth/forgot-password" color={theme.colors.primary.main} style={{
+                            fontSize: 'var(--fs-300)',
+                            alignSelf: 'flex-end',
+                            marginRight: 'var(--sp-300)'
+                        }}>Forgot password ?</Link>
+                        <ErrorMessage show={!!loginError} error={loginError} variant='normal' align='center' style={{ marginTop: '0.25rem' }} />
+                        <OAuthContainer>
+                            <p style={{
+                                color: theme.colors.black.light,
+                                fontWeight: 600,
+                                fontSize: 'var(--fs-400)',
+                                fontFamily: theme.font.family.secondary
+                            }}>
+                                Or
+                            </p>
+                            <Button iconStart={<BsGoogle />} style={{
+                                backgroundColor: '#DB4437'
+                            }} buttonProps={{
+                                onClick: (e) => { e.preventDefault(); signInWithOAuth('google') }
+                            }}>
+                                Login with Google
+                            </Button>
+                        </OAuthContainer>
+                    </FormBox>
+                )}
+            </Formik>
             <Link href='/auth/signup' color={theme.colors.white.light} style={{
                 fontFamily: theme.font.family.secondary,
                 fontWeight: 600
