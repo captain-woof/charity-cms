c6f801c6e04ad23e641ea714065dad22e5199bbb
Sohail Saha <captainwoof.official@gmail.com>
2021-11-03 10:37:01 +0530

    Shortened error messages

--- constants/auth.ts
+++ constants/auth.ts
@@ -1,14 +1,14 @@
 const AuthErrorMessages = {
     // Email and password auth
-    "auth/expired-action-code": "The password reset code you provided has expired.",
-    "auth/invalid-action-code": "The password reset code you provided is either invalid or has already been used.",
-    "auth/user-disabled": "The user account has been disabled by an administrator.",
-    "auth/user-not-found": "No user found with these credentials.",
-    "auth/weak-password": "The password you provided is too weak!",
-    "auth/email-already-in-use": "The email address is already in use by another account.",
-    "auth/invalid-email": "The email address is incorrectly formatted.",
-    "auth/wrong-password": "The password is invalid or the user does not have a password.",
-    "auth/user-token-expired":"Your credential is no longer valid. You must sign in again."
+    "auth/expired-action-code": "Reset code expired.",
+    "auth/invalid-action-code": "Reset code invalid/already used.",
+    "auth/user-disabled": "Account disabled by an administrator.",
+    "auth/user-not-found": "Incorrect email/password.",
+    "auth/weak-password": "Password too weak!",
+    "auth/email-already-in-use": "Email already in use.",
+    "auth/invalid-email": "Invalid email format.",
+    "auth/wrong-password": "Incorrect email/password.",
+    "auth/user-token-expired":"You need to sign in again."
 }
 
 export default AuthErrorMessages
