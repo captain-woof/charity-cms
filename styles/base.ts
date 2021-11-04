4670bdb501045340ae40d242849358f590744192
Sohail Saha <captainwoof.official@gmail.com>
2021-11-04 10:31:11 +0530

    Moved font imports from base css to _document

--- styles/base.ts
+++ styles/base.ts
@@ -2,9 +2,6 @@ import { createGlobalStyle, css } from "styled-components"
 
 export const GlobalStyle = createGlobalStyle`
     ${({ theme }) => (css`
-        /* FONT FAMILIES */
-        @import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
         /* CUSTOM PROPERTIES */
         :root {
             /* Constants for Base font size (fluid) */
