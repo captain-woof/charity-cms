6607e6dc282de76e42f0102f21ae8a518f69bf4e
Sohail Saha <captainwoof.official@gmail.com>
2021-11-04 14:08:57 +0530

    Individual containers imported in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+import About from "../components/containers/homepage/about"
 import Landing from "../components/containers/homepage/landing"
+import NgosWithUs from "../components/containers/homepage/ngos-with-us"
+import Stats from "../components/containers/homepage/stats"
+import { GetStaticProps } from 'next'
+import { getHomepageOrgs, getHomepageStats } from '../utils/homepage'
 
-export default function Home() {
+// Props for homepage containers
+export const getStaticProps: GetStaticProps = async () => {
+  // Fetch data from backend with util function
+  const statsDataPromise = getHomepageStats()
+  const orgsDataPromise= getHomepageOrgs()
+
+  // Get data by resolving promises
+  const [statsData, orgsData] = await Promise.all([statsDataPromise, orgsDataPromise])
+
+  return {
+    props: {
+      statsData,
+      orgsData
+    },
+    revalidate: 10
+  }
+}
+
+export default function Home({ statsData, orgsData }) {
   return (
     <>
       <Landing />
+      <Stats statsData={statsData} />
+      <About />
+      <NgosWithUs orgsData={orgsData}/>
     </>
   )
 }
