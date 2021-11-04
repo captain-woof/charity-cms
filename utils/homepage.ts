7b33bd28ce59a4f8e47fbf3da30f158aef47a2d8
Sohail Saha <captainwoof.official@gmail.com>
2021-11-04 14:09:36 +0530

    Func added to get list of orgs (homepage)

--- utils/homepage.ts
+++ utils/homepage.ts
@@ -1,3 +1,5 @@
+import { getProxiedUrl } from "./image-proxy"
+
 // Function to get stats for homepage
 interface IHomepageStats {
     totalDonationsAmount: number
@@ -5,7 +7,6 @@ interface IHomepageStats {
     totalCategories: number
     totalDonationsNum: number
 }
-
 export const getHomepageStats = async (): Promise<IHomepageStats> => {
     // TODO: Implement logic to get data from Contentful
 
@@ -16,4 +17,25 @@ export const getHomepageStats = async (): Promise<IHomepageStats> => {
         totalDonationsNum: 1203,
         totalNgos: 12
     }
+}
+
+// Function to get list of organizationss for homepage
+interface IHomepageOrg {
+    orgName: string
+    orgUrl: string
+    orgLogoUrl: string
+}
+interface IHomepageOrgs extends Array<IHomepageOrg> {
+    [index: number]: IHomepageOrg
+}
+
+export const getHomepageOrgs = async (): Promise<IHomepageOrgs> => {
+    // TODO: Implement logic to get data from Contentful
+
+    // Dummy data
+    return (new Array(8) as IHomepageOrgs).fill({
+        orgName: "Demo Org",
+        orgUrl: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        orgLogoUrl: getProxiedUrl('https://unsplash.it/250/250') 
+    })
 }
