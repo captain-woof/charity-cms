33da0e9adb71d421d81a7773155bd4317595b252
Rajarshi38 <rajarshibiswas38@gmail.com>
2022-01-01 20:26:21 +0530

    created ngo fetch apis based on email and category

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -2,133 +2,150 @@ const contentful = require("contentful");
 
 //api for fetching all the ngos
 const client = contentful.createClient({
-  space: process.env.CONTENTFUL_SPACE_ID,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN_DELIVERY,
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN_DELIVERY,
 });
 
-interface ITransaction{
-  totalDonationsNum : number,
-  totalDonationsAmount : number
+interface ITransaction {
+    totalDonationsNum: number;
+    totalDonationsAmount: number;
 }
 
-interface Ngo{
-  totalNgoCount : number,
-  ngoList : [],
+interface Ngo {
+    total: number;
+    ngos: [];
 }
 
-interface NgoCount{
-  totalNgos : number,
+interface NgoCount {
+    totalNgos: number;
 }
 
-interface TotalCategory{
-  totalCategories : number,
-
+interface TotalCategory {
+    totalCategories: number;
 }
 
 //getting the data of all the ngos
-export const ngo = async function getAllNgo()  {
-  const query = {
-    content_type: "ngo",
-    include: 10,
-    select:
-      "sys.createdAt,sys.id,fields.title,fields.description,fields.images,fields.category,fields.yearOfEstablish,fields.contact",
-
-    "fields.isVerified": false,
-  };
-  try {
-    const ngoList = await client.getEntries(query);
-    // return response.items;
-    return {
-      total: ngoList.total,
-      ngos: ngoList.items.map((ngoData) => {
-        const { title, description, images, category ,yearOfEstablish, contact } =
-          ngoData.fields;
-        const {id, createdAt: createdOn } = ngoData.sys;
-        return {
-          id,
-          title,
-          description,
-          createdOn,
-          images : images.map((image)=>({
-            alt : image.fields.title,
-            src : `https:${image.fields.file.url}`,
-            height : image.fields.file.details.image.height,
-            width : image.fields.file.details.image.width
-          })),
-          category:category.fields.categoryName,
-          yearOfEstablish,
-          contact,
-        };
-      }),
+export const ngo = async function getAllNgo({ category, userEmail }): Promise<Ngo> {
+    //query statement
+    const query = {
+        content_type: "ngo",
+        include: 10,
+        select: "sys.createdAt,sys.id,fields.title,fields.description,fields.ownerName,fields.charityEmail,fields.transactions,fields.images,fields.category,fields.yearOfEstablish,fields.contact",
+        "fields.isVerified": false,
     };
-    
-  } catch (err) {
-    return err.message;
-  }
+
+    //if category is passed
+    if (category) {
+        query["fields.category.sys.contentType.sys.id"] = "category";
+        query["fields.category.fields.categoryName[match]"] = category;
+    }
+    //if useremail has passed
+    if (userEmail) {
+        query["fields.charityEmail"] = userEmail;
+    }
+
+    /* if userEmail and category nothing is passed it will fetch all the records of the NGOs */
+    try {
+        const ngoList = await client.getEntries(query);
+        // return response.items;
+        return {
+            total: ngoList.total,
+            ngos: ngoList.items.map((ngoData) => {
+                const {
+                    title,
+                    description,
+                    ownerName,
+                    charityEmail,
+                    images,
+                    category,
+                    yearOfEstablish,
+                    contact,
+                    transactions,
+                } = ngoData.fields;
+                const { id, createdAt: createdOn } = ngoData.sys;
+                return {
+                    id,
+                    title,
+                    ownerName,
+                    charityEmail,
+                    description,
+                    createdOn,
+                    images: images.map((image) => ({
+                        alt: image.fields.title,
+                        src: `https:${image.fields.file.url}`,
+                        height: image.fields.file.details.image.height,
+                        width: image.fields.file.details.image.width,
+                    })),
+                    category: category.fields.categoryName,
+                    yearOfEstablish,
+                    contact,
+                    totalAmountRaised: transactions.reduce((totalAmount, transaction) => {
+                        return (totalAmount += transaction.fields.amount);
+                    }, 0),
+                    transactions: transactions.map((transaction) => ({
+                        transactionId: transaction.fields.id,
+                        amount: transaction.fields.amount,
+                    })),
+                };
+            }),
+        };
+    } catch (err) {
+        return err.message;
+    }
 };
 
-
 //getting the details of the transaction
-export const allTransaction = async() : Promise<ITransaction> =>{
-  const query = {
-    content_type : 'transactionDetails',
-    include : 10,
-    select : "sys.createdAt,fields.amount"
-  }
-  try{
-    const transactionList = await client.getEntries(query);
-    return{
-      totalDonationsNum : transactionList.total,      
-      totalDonationsAmount : transactionList.items.reduce((totalAmount,transaction)=>{
-        
-        return totalAmount + transaction.fields.amount;
-        
-      },0),
-      
-
+export const allTransaction = async (): Promise<ITransaction> => {
+    const query = {
+        content_type: "transactionDetails",
+        include: 10,
+        select: "sys.createdAt,fields.amount",
+    };
+    try {
+        const transactionList = await client.getEntries(query);
+        return {
+            totalDonationsNum: transactionList.total,
+            totalDonationsAmount: transactionList.items.reduce(
+                (totalAmount, transaction) => {
+                    return totalAmount + transaction.fields.amount;
+                },
+                0
+            ),
+        };
+    } catch (err) {
+        return err.message;
     }
-  }catch(err){
-    return err.message;
-  }
-}
+};
 
-
-
-export const ngoCount = async() : Promise<NgoCount> =>{
-  const query = {
-    content_type : 'ngo',
-    include : 10
-  }
-try{
-  const ngoList = await client.getEntries(query);
-  return {
-    totalNgos : ngoList.total
-  }
-}catch(err){
-  return err.mnessage;
-}
-    
-}
-
-export const categoryCount = async() : Promise<TotalCategory> =>{
-  const query = {
-    content_type : 'category',
-    include :10
-  }
-
-  try{
-    const categoryList = await client.getEntries(query);
-    return { 
-      totalCategories : categoryList.total
+export const ngoCount = async (): Promise<NgoCount> => {
+    const query = {
+        content_type: "ngo",
+        include: 10,
+    };
+    try {
+        const ngoList = await client.getEntries(query);
+        return {
+            totalNgos: ngoList.total,
+        };
+    } catch (err) {
+        return err.mnessage;
     }
-  }catch(err){
-    return err.message;
-  }
-}
+};
 
+// fetching how many categories of NGOs have registered so far
 
+export const categoryCount = async (): Promise<TotalCategory> => {
+    const query = {
+        content_type: "category",
+        include: 10,
+    };
 
-
-
-
-
+    try {
+        const categoryList = await client.getEntries(query);
+        return {
+            totalCategories: categoryList.total,
+        };
+    } catch (err) {
+        return err.message;
+    }
+};
--- pages/api/fetchNgos.ts
+++ pages/api/fetchNgos.ts
@@ -1,12 +1,19 @@
 import { ngo } from "../../lib/contentful";
-import type {NextApiRequest,NextApiResponse} from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req : NextApiRequest,res : NextApiResponse){
-  
-  try{
-    const ngoData : any  = await ngo();
-    res.status(200).json(ngoData);
-  }catch(e){
-    res.status(500).json(e.message);
-  }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    /**
+     * /api/fetchNgos?userEmail=......
+     * /api/fetchNgos?category=........
+     */
+
+    try {
+        const ngoData = await ngo({
+            category: req.query.category,
+            userEmail: req.query.userEmail,
+        });
+        res.status(200).json(ngoData);
+    } catch (e) {
+        res.status(500).json(e.message);
+    }
 }
