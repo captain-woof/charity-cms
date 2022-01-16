81813ad456ea276f3a5a571217e4e31415d5cb8e
Sohail Saha <captainwoof.official@gmail.com>
2022-01-16 21:27:20 +0530

    API routes are protected

--- pages/api/createNgos.ts
+++ pages/api/createNgos.ts
@@ -5,6 +5,8 @@ import { getCategories } from "../../lib/contentful";
 // import { promises, readFileSync } from "fs";
 import { Data } from "../../types/ngo";
 import fs from "fs";
+import { parseCookies } from "nookies";
+import { verifyIdToken } from "../../utils/auth-server";
 
 export const config = {
     api: {
@@ -24,57 +26,68 @@ const capitalize = (text: string): string => {
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         if (req.method === "POST") {
-            const data: Data = await new Promise((resolve, reject) => {
-                const form = new formidable.IncomingForm();
-                form.parse(req, (err, fields, files) => {
-                    if (err) {
-                        return reject(err);
-                    }
-                    resolve({ fields, files });
+            // Check if user is authenticated
+            const cookies = parseCookies({ req })
+            if (cookies.token) {
+                const { status } = await verifyIdToken(cookies.token)
+                if (status) { // Only if user is authenticated
+                    const data: Data = await new Promise((resolve, reject) => {
+                        const form = new formidable.IncomingForm();
+                        form.parse(req, (err, fields, files) => {
+                            if (err) {
+                                return reject(err);
+                            }
+                            resolve({ fields, files });
+                        });
+                    });
+
+                    const image = data.files.image as formidable.File;
+                    const pdf = data.files.verificationPdf as formidable.File;
+                    const contentOfImage = fs.readFileSync(image.filepath);
+                    const contentOfPdf = fs.readFileSync(pdf.filepath);
+                    let imageName = image.originalFilename;
+                    let imageType = image.mimetype;
+                    let pdfName = pdf.originalFilename;
+                    let pdfType = pdf.mimetype;
+
+                    const [imageId, pdfId] = await Promise.all([
+                        createAsset(
+                            { name: imageName, type: imageType },
+                            contentOfImage
+                        ), createAsset(
+                            { name: pdfName, type: pdfType },
+                            contentOfPdf
+                        )])
+
+                    const category = data.fields.category as string;
+                    const response = await getCategories(category);
+                    let categoryId: string;
+                    // if (response.total === 0) {
+                    //     categoryId = await createCategory(category);
+
+                    categoryId = response.categories[0].id;
+
+                    const newNgo = {
+                        title: data.fields.title as string,
+                        description: data.fields.description as string,
+                        yearOfEstablish: parseInt(data.fields.yearOfEstablish as string),
+                        charityEmail: data.fields.charityEmail as string,
+                        contact: data.fields.contact as string,
+                        ownerName: data.fields.ownerName as string,
+                        categoryId: categoryId,
+                        verificationId: pdfId,
+                        imageId: imageId,
+                    };
+
+                    const ngoCreated = await createNgo(newNgo);
+
+                    res.status(201).json(ngoCreated);
+                }
+            } else { // If not unauthenticated
+                res.status(401).json({
+                    message: "You must be authenticated!",
                 });
-            });
-
-            const image = data.files.image as formidable.File;
-            const pdf = data.files.verificationPdf as formidable.File;
-            const contentOfImage = fs.readFileSync(image.filepath);
-            const contentOfPdf = fs.readFileSync(pdf.filepath);
-            let imageName = image.originalFilename;
-            let imageType = image.mimetype;
-            let pdfName = pdf.originalFilename;
-            let pdfType = pdf.mimetype;
-
-            const [imageId, pdfId] = await Promise.all([
-                createAsset(
-                    { name: imageName, type: imageType },
-                    contentOfImage
-                ), createAsset(
-                    { name: pdfName, type: pdfType },
-                    contentOfPdf
-                )])
-
-            const category = data.fields.category as string;
-            const response = await getCategories(category);
-            let categoryId: string;
-            // if (response.total === 0) {
-            //     categoryId = await createCategory(category);
-
-            categoryId = response.categories[0].id;
-
-            const newNgo = {
-                title: data.fields.title as string,
-                description: data.fields.description as string,
-                yearOfEstablish: parseInt(data.fields.yearOfEstablish as string),
-                charityEmail: data.fields.charityEmail as string,
-                contact: data.fields.contact as string,
-                ownerName: data.fields.ownerName as string,
-                categoryId: categoryId,
-                verificationId: pdfId,
-                imageId: imageId,
-            };
-
-            const ngoCreated = await createNgo(newNgo);
-
-            res.status(201).json(ngoCreated);
+            }
         }
     } catch (e) {
         res.status(500).json(e.message);
--- pages/api/createTransaction.ts
+++ pages/api/createTransaction.ts
@@ -2,6 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Data } from "../../types/ngo";
 import formidable from "formidable";
 import { createTransaction } from "../../lib/management";
+import { parseCookies } from "nookies";
+import { verifyIdToken } from "../../utils/auth-server";
 //default bodyparser false
 export const config = {
     api: {
@@ -13,23 +15,34 @@ export const config = {
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
         try {
-            const data: Data = await new Promise((resolve, reject) => {
-                const form = new formidable.IncomingForm();
-                form.parse(req, (err, fields, files) => {
-                    if (err) {
-                        return reject(err);
-                    }
-                    resolve({ fields, files });
+            // Check if user is authenticated
+            const cookies = parseCookies({ req })
+            if (cookies.token) {
+                const { status } = await verifyIdToken(cookies.token)
+                if (status) { // Only if user is authenticated
+                    const data: Data = await new Promise((resolve, reject) => {
+                        const form = new formidable.IncomingForm();
+                        form.parse(req, (err, fields, files) => {
+                            if (err) {
+                                return reject(err);
+                            }
+                            resolve({ fields, files });
+                        });
+                    });
+
+                    let ngoSlug = data.fields.slug as string;
+                    let transactionId = data.fields.id as string;
+                    let amount = parseFloat(data.fields.amount as string);
+
+                    await createTransaction(ngoSlug, transactionId, amount);
+
+                    res.status(201).json({ message: "success! new Transaction created" });
+                }
+            } else { // If not unauthenticated
+                res.status(401).json({
+                    message: "You must be authenticated!",
                 });
-            });
-
-            let ngoSlug = data.fields.slug as string;
-            let transactionId = data.fields.id as string;
-            let amount = parseFloat(data.fields.amount as string);
-
-            await createTransaction(ngoSlug, transactionId, amount);
-
-            res.status(201).json({ message: "success! new Transaction created" });
+            }
         } catch (error) {
             res.status(500).json(error.message);
         }
